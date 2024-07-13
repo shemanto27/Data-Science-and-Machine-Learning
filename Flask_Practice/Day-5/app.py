3e7e1ed60fe85996688de1676849f88bbce2c6ca
shemanto27 <shemantosharkarofficial@gmail.com>
2024-07-13 22:53:59 +0600

    flask app done

--- Flask_Practice/Day-5/app.py
+++ Flask_Practice/Day-5/app.py
@@ -9,8 +9,9 @@ def home():
 
 @app.route("/report/<int:score>")
 def report(score):
-    return render_template('report.html',score=score)
+    return render_template('report.html',total_score=score) #Pass score to the report template
 
+# total_score before the = sign is the name of the variable that will be available in the template, and the score after the = sign is the value being passed to the template (which is the parameter received by the function)
 
 
 @app.route('/result', methods=['POST', 'GET'])
@@ -22,8 +23,8 @@ def result():
         English = float(data["English"])
         Math = float(data["Math"])
         total = int(Bangla + English + Math)
-        return redirect(url_for("report", score=total))
-
+        return redirect(url_for("report", score=total)) #Pass score to redirect call
+# score=total is used to pass the total variable's value as the score parameter to the report endpoint.
 
 if __name__ == '__main__':
     app.run(debug=True)
